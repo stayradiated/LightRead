cac30ecbf7bebb519397cfd02cd75b99d8deb503
Simonas Kazlauskas <simonas@kazlauskas.me>
2012-08-09 13:42:21 +0300

    Remove unused code

--- Ubuntu/setup.py
+++ Ubuntu/setup.py
@@ -68,30 +68,11 @@ def update_config(values = {}):
     return oldvalues
 
 
-# def update_desktop_file(datadir):
-#
-#     try:
-#         fin = file('lightread.desktop.in', 'r')
-#         fout = file(fin.name + '.new', 'w')
-#
-#         for line in fin:
-#             if 'Icon=' in line:
-#                 line = "Icon=%s\n" % (datadir + 'media/lightread.png')
-#             fout.write(line)
-#         fout.flush()
-#         fout.close()
-#         fin.close()
-#         os.rename(fout.name, fin.name)
-#     except (OSError, IOError), e:
-#         print ("ERROR: Can't find lightread.desktop.in")
-#         sys.exit(1)
-
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
         values = {'__lightread_data_directory__': "'%s'" % (self.prefix + '/share/lightread/'),
                   '__version__': "'%s'" % self.distribution.get_version()}
         previous_values = update_config(values)
-        # update_desktop_file(self.prefix + '/share/lightread/')
         DistUtilsExtra.auto.install_auto.run(self)
         update_config(previous_values)
 
